b42c6a404094757c261b8bde9605f079d0b5f7a9
Angel uriel dominguez medina <urielmedina7u7@outlook.com>
2025-05-02 11:40:27 -0600

    Update header.php

--- sistema_agencia/private/plantilla/header.php
+++ sistema_agencia/private/plantilla/header.php
@@ -53,26 +53,26 @@ class Header
         echo '    });';
         echo '}';
         echo '</script>';
+
         // Cargar archivos desde CDN por defecto
-        
         //DATA TABLE SIMPLE
         echo '<link id="datatable-css" href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet">';
-       
+
         // BOOSTRAP
         echo '<link id="bootstrap-css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">';
         echo '<script id="bootstrap-js" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>';
-        
+
         // CHART.JS
         echo '<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>';
         echo '<link id="chart-css" href="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.css" rel="stylesheet">';
-       
+
         // JQUERY 3.6.0
         echo '<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>';
-       
+
         // SWEETALERT 2
         echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
         echo '<link id="sweetalert-css" href="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.7/dist/sweetalert2.min.css" rel="stylesheet">';
-       
+
         // BOOSTRAP ICONS
         echo '<link id="icons-css" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">';
 
