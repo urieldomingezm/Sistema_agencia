3dfaf992474ab6e0bc37fe1a6e83306f44eef28a
Angel uriel dominguez medina <urielmedina7u7@outlook.com>
2025-05-02 09:35:18 -0600

    Update header.php

--- sistema_agencia/private/plantilla/header.php
+++ sistema_agencia/private/plantilla/header.php
@@ -33,12 +33,12 @@ class Header
         echo '<link rel="icon" type="image/x-icon" href="/public/custom/custom_radio/img/dj.jpg">';
 
         // Google Analytics
-        echo '<script async src="https://www.googletagmanager.com/gtag/js?id=G-GPVYVF599C"></script>';
+        echo '<script async src="https://www.googletagmanager.com/gtag/js?id=G-32NSVX1ZQD"></script>';
         echo '<script>';
         echo 'window.dataLayer = window.dataLayer || [];';
         echo 'function gtag(){dataLayer.push(arguments);}';
         echo 'gtag("js", new Date());';
-        echo 'gtag("config", "G-GPVYVF599C");';
+        echo 'gtag("config", "G-32NSVX1ZQD");';
         echo '</script>';
 
         // Cargar archivos desde CDN por defecto
