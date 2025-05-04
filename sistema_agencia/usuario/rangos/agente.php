dbf62e661ad271eba5050873ccf12c3a321aeb99
TheRussian7u7 <urielmedina7u7@outlook.com>
2025-05-04 14:03:11 -0600

    Update agente.php

--- sistema_agencia/usuario/rangos/agente.php
+++ sistema_agencia/usuario/rangos/agente.php
@@ -9,7 +9,7 @@
                 <div class="tab-content" id="pills-tabContent">
                     <div class="tab-pane fade show active" id="misiones" role="tabpanel" aria-labelledby="misiones-tab" tabindex="0">
                         <h4>Ascienden cada 30 minutos</h4>
-                        <p>Misiones: 4</p>
+                        <p>Misiones: 9</p>
                         <div class="table-responsive">
                             <table class="table table-striped text-center table-bordered table-borderless">
                                 <thead>
@@ -21,19 +21,39 @@
                                 <tbody>
                                     <tr>
                                         <th scope="row">1</th>
-                                        <td>SNY- Aspirante</td>
+                                        <td>AGE- Iniciado I</td>
                                     </tr>
                                     <tr>
                                         <th scope="row">2</th>
-                                        <td>SNY- Agente -I</td>
+                                        <td>AGE- Novato H</td>
                                     </tr>
                                     <tr>
                                         <th scope="row">3</th>
-                                        <td>SNY- Agente -II</td>
+                                        <td>AGE- Auxiliar G</td>
                                     </tr>
                                     <tr>
                                         <th scope="row">4</th>
-                                        <td>SNY- Agente -III</td>
+                                        <td>AGE- Ayudante F</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">5</th>
+                                        <td>AGE- Junior E</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">6</th>
+                                        <td>AGE- Intermedio D</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">7</th>
+                                        <td>AGE- Avanzado C</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">8</th>
+                                        <td>AGE- Experto B</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">9</th>
+                                        <td>AGE- Jefe A</td>
                                     </tr>
                                 </tbody>
                             </table>
