779fd130451896d76d98bc89fd4957ab41cdf371
Angel uriel dominguez medina <urielmedina7u7@outlook.com>
2025-05-07 12:26:28 -0600

    POO

--- sistema_agencia/usuario/GSTM.php
+++ sistema_agencia/usuario/GSTM.php
@@ -1,69 +1,88 @@
-<?php 
-// RUTA PROCESO PARA MOSTRAR TODOS LOS USUARIOS EN LA TABLA DE GESTION DE TIEMPOS
+<?php
+// PROCESO PARA MOSTRAR TIEMPOS DE USUARIOS
 require_once(GESTION_TIEMPO_PATCH . 'mostrar_usuarios.php');
 
-// RUTA DE MODALES PARA GESTION DE TIEMPOS PARA ACCIONES DE GESTION DE TIEMPOS: AUSENTE, PAUSAR, COMPLETADO, INICIAR Y FINALIZAR
-?>
+// MODALES PARA ACCIONES DE TIEMPOS DE USUARIOS 
 
-<table id="datatable" class="datatable-table table table-striped">
-    <thead>
-        <tr>
-            <th>Usuario</th>
-            <th>Status</th>
-            <th>Restado</th>
-            <th>Acumulado</th>
-            <th>Transcurrido</th>
-            <th>Registro</th>
-            <th>Encargado</th>
-            <th>Acciones</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach($GLOBALS['tiempos'] as $tiempo): ?>
-        <tr>
-            <td><?= $tiempo['nombre_habbo'] ?? $tiempo['codigo_time'] ?></td>
-            <td>
-                <span class="badge <?php
-                    switch($tiempo['tiempo_status']) {
-                        case 'corriendo':
-                            echo 'bg-success';
-                            break;
-                        case 'pausado':
-                            echo 'bg-warning';
-                            break;
-                        case 'ausente':
-                            echo 'bg-danger';
-                            break;
-                        case 'completado':
-                            echo 'bg-purple';
-                            break;
-                        default:
-                            echo 'bg-secondary';
-                    }
-                ?>">
-                    <?= $tiempo['tiempo_status'] ?>
-                </span>
-            </td>
-            <td><?= $tiempo['tiempo_restado'] ?></td>
-            <td><?= $tiempo['tiempo_acumulado'] ?></td>
-            <td><?= $tiempo['tiempo_transcurrido'] ?></td>
-            <td><?= date('Y-m-d', strtotime($tiempo['tiempo_fecha_registro'])) ?></td>
-            <td><?= $tiempo['tiempo_encargado_usuario'] ?? 'No disponible' ?></td>
-            <td>
-                <div class="dropdown">
-                    <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="actionDropdown<?= $tiempo['codigo_time'] ?>" data-bs-toggle="dropdown" aria-expanded="false">
-                        Acciones
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="actionDropdown<?= $tiempo['codigo_time'] ?>">
-                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#iniciarModal" data-id="<?= $tiempo['codigo_time'] ?>">Iniciar</a></li>
-                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#pausarModal" data-id="<?= $tiempo['codigo_time'] ?>">Pausar</a></li>
-                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#ausenteModal" data-id="<?= $tiempo['codigo_time'] ?>">Ausente</a></li>
-                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#completadoModal" data-id="<?= $tiempo['codigo_time'] ?>">Completado</a></li>
-                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#finalizarModal" data-id="<?= $tiempo['codigo_time'] ?>">Finalizar</a></li>
-                    </ul>
-                </div>
-            </td>
-        </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+class TimeManagementTable {
+    private $tiempos;
+    
+    public function __construct($tiempos) {
+        $this->tiempos = $tiempos;
+    }
+    
+    private function getStatusBadgeClass($status) {
+        $statusClasses = [
+            'corriendo' => 'bg-success',
+            'pausado' => 'bg-warning',
+            'ausente' => 'bg-danger',
+            'completado' => 'bg-purple'
+        ];
+        return $statusClasses[$status] ?? 'bg-secondary';
+    }
+    
+    private function renderActionDropdown($codigoTime) {
+        $actions = [
+            'iniciar' => 'Iniciar',
+            'pausar' => 'Pausar',
+            'ausente' => 'Ausente',
+            'completado' => 'Completado',
+            'finalizar' => 'Finalizar'
+        ];
+        
+        $html = '<div class="dropdown">';
+        $html .= '<button class="btn btn-secondary btn-sm dropdown-toggle" type="button" ';
+        $html .= 'id="actionDropdown' . $codigoTime . '" data-bs-toggle="dropdown" aria-expanded="false">';
+        $html .= 'Acciones</button><ul class="dropdown-menu" aria-labelledby="actionDropdown' . $codigoTime . '">';
+        
+        foreach ($actions as $action => $label) {
+            $html .= '<li><a class="dropdown-item" href="#" data-bs-toggle="modal" ';
+            $html .= 'data-bs-target="#' . $action . 'Modal" data-id="' . $codigoTime . '">' . $label . '</a></li>';
+        }
+        
+        $html .= '</ul></div>';
+        return $html;
+    }
+    
+    public function render() {
+        ?>
+        <table id="datatable" class="datatable-table table table-striped">
+            <thead>
+                <tr>
+                    <th>Usuario</th>
+                    <th>Status</th>
+                    <th>Restado</th>
+                    <th>Acumulado</th>
+                    <th>Transcurrido</th>
+                    <th>Registro</th>
+                    <th>Encargado</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($this->tiempos as $tiempo): ?>
+                <tr>
+                    <td><?= $tiempo['nombre_habbo'] ?? $tiempo['codigo_time'] ?></td>
+                    <td>
+                        <span class="badge <?= $this->getStatusBadgeClass($tiempo['tiempo_status']) ?>">
+                            <?= $tiempo['tiempo_status'] ?>
+                        </span>
+                    </td>
+                    <td><?= $tiempo['tiempo_restado'] ?></td>
+                    <td><?= $tiempo['tiempo_acumulado'] ?></td>
+                    <td><?= $tiempo['tiempo_transcurrido'] ?></td>
+                    <td><?= date('Y-m-d', strtotime($tiempo['tiempo_fecha_registro'])) ?></td>
+                    <td><?= $tiempo['tiempo_encargado_usuario'] ?? 'No disponible' ?></td>
+                    <td><?= $this->renderActionDropdown($tiempo['codigo_time']) ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <?php
+    }
+}
+
+// Initialize and render the table
+$timeTable = new TimeManagementTable($GLOBALS['tiempos']);
+$timeTable->render();
+?>
