c8cbd36386583ccb9f54c9c7dd70191192ecc8ea
TheRussian7u7 <urielmedina7u7@outlook.com>
2025-05-31 18:32:35 -0600

    Update mostrar_requisitos.php

--- sistema_agencia/private/procesos/gestion_pagas/mostrar_requisitos.php
+++ sistema_agencia/private/procesos/gestion_pagas/mostrar_requisitos.php
@@ -13,10 +13,19 @@ class RequisitoManager
 
     public function obtenerRequisitosPendientes()
     {
-        $query = "SELECT user_codigo_time, is_completed, last_updated, requirement_name 
-                  FROM gestion_requisitos 
-                  WHERE is_completed = FALSE 
-                  ORDER BY last_updated DESC";
+        $query = "
+        SELECT gr.*
+        FROM gestion_requisitos gr
+        INNER JOIN (
+            SELECT user_codigo_time, MAX(last_updated) AS ultima_fecha
+            FROM gestion_requisitos
+            WHERE is_completed = FALSE
+            GROUP BY user_codigo_time
+        ) ultimos
+        ON gr.user_codigo_time = ultimos.user_codigo_time AND gr.last_updated = ultimos.ultima_fecha
+        WHERE gr.is_completed = FALSE
+        ORDER BY gr.last_updated DESC
+    ";
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -26,5 +35,3 @@ class RequisitoManager
 $db = new Database();
 $requisitoManager = new RequisitoManager($db);
 $requisitos = $requisitoManager->obtenerRequisitosPendientes();
-
-?>
