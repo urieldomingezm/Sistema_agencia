81ded8d43520eeaff4e695f4438eef578f15a4e0
TheRussian7u7 <urielmedina7u7@outlook.com>
2025-06-04 19:15:54 -0600

    Update dashboard.php

---
+++ sistema_agencia/usuario/administrativo/dashboard.php
@@ -0,0 +1,9 @@
+
+<meta name="keywords" content="Inicio de usuario, atras">
+
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');?>
+
+<?php
+require_once(BODY_HOME_PATH . 'home_usuario.php');
+?>
