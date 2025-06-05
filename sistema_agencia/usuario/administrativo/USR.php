bdd803d6168d0c0cfc96ecfb3a0a730a06a67e1a
TheRussian7u7 <urielmedina7u7@outlook.com>
2025-06-04 19:49:12 -0600

    Update USR.php

--- sistema_agencia/usuario/administrativo/USR.php
+++ sistema_agencia/usuario/administrativo/USR.php
@@ -3,47 +3,6 @@
 <body class="bg-light">
     <div class="container-fluid">
         <div class="row flex-nowrap">
-            <!-- Sidebar -->
-            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark min-vh-100">
-                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white">
-                    <a href="#" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                        <span class="fs-4 d-none d-sm-inline"><i class="bi bi-speedometer2"></i> Dashboard</span>
-                    </a>
-                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link text-white">
-                                <i class="bi bi-house-door"></i> <span class="ms-2 d-none d-sm-inline">Inicio</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link text-white">
-                                <i class="bi bi-people"></i> <span class="ms-2 d-none d-sm-inline">Usuarios</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link text-white">
-                                <i class="bi bi-chat-dots"></i> <span class="ms-2 d-none d-sm-inline">Chat</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link text-white">
-                                <i class="bi bi-gear"></i> <span class="ms-2 d-none d-sm-inline">Configuración</span>
-                            </a>
-                        </li>
-                    </ul>
-                    <hr>
-                    <div class="dropdown pb-4">
-                        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown">
-                            <img src="https://via.placeholder.com/32" alt="user" width="32" height="32" class="rounded-circle">
-                            <span class="d-none d-sm-inline mx-1">Admin</span>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                            <li><a class="dropdown-item" href="#">Perfil</a></li>
-                            <li><a class="dropdown-item" href="#">Salir</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
             <!-- Main Content -->
             <div class="col py-3">
                 <div class="container">
