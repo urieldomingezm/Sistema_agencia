da399509fd9ae2d0f63c0368b6eb2fd7f08736fc
Angel uriel dominguez medina <urielmedina7u7@outlook.com>
2025-06-06 14:22:26 -0600

    Update perfil.php

--- sistema_agencia/usuario/app/perfil.php
+++ sistema_agencia/usuario/app/perfil.php
@@ -1,76 +1,78 @@
-<ion-header>
-  <ion-toolbar color="primary">
-    <ion-title>Mi Perfil</ion-title>
-  </ion-toolbar>
-</ion-header>
+<ion-app>
+  <ion-header>
+    <ion-toolbar color="dark">
+      <ion-title>Mi Perfil</ion-title>
+    </ion-toolbar>
+  </ion-header>
 
-<ion-content class="ion-padding">
-  <ion-card>
-    <ion-card-header class="ion-text-center">
-      <ion-avatar style="width: 100px; height: 100px; margin: 0 auto;">
-        <img src="https://i.pravatar.cc/300" alt="Avatar de usuario">
-      </ion-avatar>
-      <ion-card-title>Usuario Ejemplo</ion-card-title>
-      <ion-badge color="primary">Agente</ion-badge>
-    </ion-card-header>
-  </ion-card>
-  
-  <ion-list lines="full">
-    <ion-item>
-      <ion-icon name="mail" slot="start"></ion-icon>
-      <ion-label>Correo Electrónico</ion-label>
-      <ion-note slot="end">usuario@ejemplo.com</ion-note>
-    </ion-item>
-    
-    <ion-item>
-      <ion-icon name="calendar" slot="start"></ion-icon>
-      <ion-label>Fecha de Ingreso</ion-label>
-      <ion-note slot="end">15/03/2023</ion-note>
-    </ion-item>
-    
-    <ion-item>
-      <ion-icon name="time" slot="start"></ion-icon>
-      <ion-label>Tiempo Total</ion-label>
-      <ion-note slot="end">245 horas</ion-note>
-    </ion-item>
-    
-    <ion-item>
-      <ion-icon name="trophy" slot="start"></ion-icon>
-      <ion-label>Último Ascenso</ion-label>
-      <ion-note slot="end">Hace 2 meses</ion-note>
-    </ion-item>
-  </ion-list>
-  
-  <ion-grid class="ion-margin-top">
-    <ion-row>
-      <ion-col>
-        <ion-button expand="block" fill="outline">
-          <ion-icon name="settings" slot="start"></ion-icon>
-          Configuración
-        </ion-button>
-      </ion-col>
-    </ion-row>
-  </ion-grid>
-</ion-content>
+  <ion-content color="dark">
+    <ion-card color="dark">
+      <ion-card-header>
+        <ion-avatar>
+          <img src="https://i.pravatar.cc/300" alt="Avatar de usuario">
+        </ion-avatar>
+        <ion-card-title>Usuario Ejemplo</ion-card-title>
+        <ion-badge color="medium">Agente</ion-badge>
+      </ion-card-header>
+    </ion-card>
 
-<ion-footer>
-  <ion-tabs>
-    <ion-tab-bar slot="bottom">
-      <ion-tab-button tab="inicio" href="?page=inicio">
-        <ion-icon name="home"></ion-icon>
-        <ion-label>Inicio</ion-label>
-      </ion-tab-button>
-      
-      <ion-tab-button tab="perfil" selected>
-        <ion-icon name="person"></ion-icon>
-        <ion-label>Perfil</ion-label>
-      </ion-tab-button>
-      
-      <ion-tab-button tab="notificaciones" href="?page=notificaciones">
-        <ion-icon name="notifications"></ion-icon>
-        <ion-label>Notificaciones</ion-label>
-        <ion-badge color="danger">2</ion-badge>
-      </ion-tab-button>
-    </ion-tab-bar>
-  </ion-tabs>
-</ion-footer>
+    <ion-list lines="full" color="dark">
+      <ion-item color="dark">
+        <ion-icon name="mail" slot="start"></ion-icon>
+        <ion-label>Correo Electrónico</ion-label>
+        <ion-note slot="end">usuario@ejemplo.com</ion-note>
+      </ion-item>
+
+      <ion-item color="dark">
+        <ion-icon name="calendar" slot="start"></ion-icon>
+        <ion-label>Fecha de Ingreso</ion-label>
+        <ion-note slot="end">15/03/2023</ion-note>
+      </ion-item>
+
+      <ion-item color="dark">
+        <ion-icon name="time" slot="start"></ion-icon>
+        <ion-label>Tiempo Total</ion-label>
+        <ion-note slot="end">245 horas</ion-note>
+      </ion-item>
+
+      <ion-item color="dark">
+        <ion-icon name="trophy" slot="start"></ion-icon>
+        <ion-label>Último Ascenso</ion-label>
+        <ion-note slot="end">Hace 2 meses</ion-note>
+      </ion-item>
+    </ion-list>
+
+    <ion-grid>
+      <ion-row>
+        <ion-col>
+          <ion-button expand="block" fill="outline" color="light">
+            <ion-icon name="settings" slot="start"></ion-icon>
+            Configuración
+          </ion-button>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+  </ion-content>
+
+  <ion-footer>
+    <ion-tabs>
+      <ion-tab-bar slot="bottom" color="dark">
+        <ion-tab-button tab="inicio" href="?page=inicio">
+          <ion-icon name="home"></ion-icon>
+          <ion-label>Inicio</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="perfil" selected>
+          <ion-icon name="person"></ion-icon>
+          <ion-label>Perfil</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="notificaciones" href="?page=notificaciones">
+          <ion-icon name="notifications"></ion-icon>
+          <ion-label>Notificaciones</ion-label>
+          <ion-badge color="danger">2</ion-badge>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-footer>
+</ion-app>
