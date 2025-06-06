7eb44daaceb37096b29df0b2517c720c6947e110
TheRussian7u7 <urielmedina7u7@outlook.com>
2025-06-05 23:40:55 -0600

    Update index.php

--- sistema_agencia/usuario/app/index.php
+++ sistema_agencia/usuario/app/index.php
@@ -4,7 +4,8 @@ $pageTitle = "Agencia Shein APP - Inicio";
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once(TEMPLATES_APP_PATH . 'header_app.php');
 
-require_once(TABS_APP_PATH . 'tab_bajos.php');
 
 require_once(TEMPLATES_APP_PATH . 'footer_app.php');
+
+require_once(TABS_APP_PATH . 'tab_bajos.php');
 ?>
