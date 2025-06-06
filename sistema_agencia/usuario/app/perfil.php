3280753919271d17bbf3aca783796af57e85f0d5
TheRussian7u7 <urielmedina7u7@outlook.com>
2025-06-06 07:38:41 -0600

    Update perfil.php

--- sistema_agencia/usuario/app/perfil.php
+++ sistema_agencia/usuario/app/perfil.php
@@ -1,14 +1,16 @@
-<div class="app-page">
-    <div class="profile-header">
-        <ion-avatar class="profile-avatar">
-            <img src="/assets/images/avatars/<?php echo $_SESSION['user_id']; ?>.jpg" alt="Avatar" 
-                 onerror="this.src='/assets/images/avatars/default.jpg'">
-        </ion-avatar>
-        <h2 class="profile-name"><?php echo htmlspecialchars($_SESSION['username']); ?></h2>
-        <ion-badge color="primary"><?php echo htmlspecialchars($controller->userRango); ?></ion-badge>
-    </div>
+<ion-content>
+    <ion-card>
+        <ion-card-header>
+            <ion-avatar>
+                <img src="/assets/images/avatars/<?php echo $_SESSION['user_id']; ?>.jpg" alt="Avatar" 
+                     onerror="this.src='/assets/images/avatars/default.jpg'">
+            </ion-avatar>
+            <ion-card-title><?php echo htmlspecialchars($_SESSION['username']); ?></ion-card-title>
+            <ion-badge color="primary"><?php echo htmlspecialchars($controller->userRango); ?></ion-badge>
+        </ion-card-header>
+    </ion-card>
     
-    <ion-list class="profile-details">
+    <ion-list>
         <ion-item>
             <ion-icon name="mail" slot="start"></ion-icon>
             <ion-label>Correo Electrónico</ion-label>
@@ -34,44 +36,12 @@
         </ion-item>
     </ion-list>
     
-    <div class="profile-actions">
-        <ion-button expand="block" fill="outline" href="?page=configuracion">
-            <ion-icon name="settings" slot="start"></ion-icon>
-            Configuración
-        </ion-button>
-    </div>
-</div>
-
-<style>
-    .profile-header {
-        text-align: center;
-        padding: 20px 0;
-    }
-    
-    .profile-avatar {
-        width: 100px;
-        height: 100px;
-        margin: 0 auto 10px;
-    }
-    
-    .profile-name {
-        margin: 5px 0;
-        font-size: 1.4rem;
-    }
-    
-    .profile-details {
-        margin-top: 20px;
-        background: transparent;
-    }
-    
-    .profile-details ion-item {
-        --padding-start: 0;
-        --inner-padding-end: 0;
-        --background: transparent;
-    }
-    
-    .profile-actions {
-        margin-top: 30px;
-        padding: 0 16px;
-    }
-</style>
+    <ion-footer>
+        <ion-toolbar>
+            <ion-button expand="block" fill="outline" href="?page=configuracion">
+                <ion-icon name="settings" slot="start"></ion-icon>
+                Configuración
+            </ion-button>
+        </ion-toolbar>
+    </ion-footer>
+</ion-content>
