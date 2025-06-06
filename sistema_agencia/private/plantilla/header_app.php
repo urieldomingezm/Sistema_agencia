f1d8a26c8a0aee60af35a0b868f457d7ec99bd18
Angel uriel dominguez medina <urielmedina7u7@outlook.com>
2025-06-06 16:17:36 -0600

    Update header_app.php

--- sistema_agencia/private/plantilla/header_app.php
+++ sistema_agencia/private/plantilla/header_app.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($pageTitle) ? $pageTitle : 'Agencia Shein APP'; ?></title>
-    
+
 
     <script type="module" src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js"></script>
     <script nomodule src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ionic/core/css/ionic.bundle.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ionic/core/css/ionic.bundle.css" />
     <style>
         ion-content {
             --overflow: auto;
             --offset-top: 56px;
             --offset-bottom: 56px;
         }
-        
-        html, body {
+
+        html,
+        body {
             height: 100%;
         }
-        
+
         ion-app {
             min-height: 100%;
         }
     </style>
 </head>
+
 <body class="ion-padding" color="dark">
     <ion-app>
 
@@ -38,3 +41,4 @@
                 </ion-buttons>
             </ion-toolbar>
         </ion-header>
+        <br>
