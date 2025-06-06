c9fc911a4daf3ac0fd62c454727f195b2f3597d6
Angel uriel dominguez medina <urielmedina7u7@outlook.com>
2025-06-06 15:40:16 -0600

    Update tab_bajos.php

--- sistema_agencia/private/tabs/tab_bajos.php
+++ sistema_agencia/private/tabs/tab_bajos.php
@@ -1,29 +1,55 @@
 <ion-tabs>
-    <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="inicio" href="?page=inicio">
-            <ion-icon name="home"></ion-icon>
-            <ion-label>Inicio</ion-label>
-        </ion-tab-button>
-        
-        <ion-tab-button tab="perfil" href="?page=perfil">
-            <ion-icon name="person"></ion-icon>
-            <ion-label>Perfil</ion-label>
-        </ion-tab-button>
-        
-        <ion-tab-button tab="notificaciones" href="?page=notificaciones">
-            <ion-icon name="notifications"></ion-icon>
-            <ion-label>Notificaciones</ion-label>
-            <ion-badge color="danger">2</ion-badge>
-        </ion-tab-button>
-        
-        <ion-tab-button tab="tiempos" href="?page=tiempos">
-            <ion-icon name="time"></ion-icon>
-            <ion-label>Tiempos</ion-label>
-        </ion-tab-button>
-        
-        <ion-tab-button tab="configuracion" href="?page=configuracion">
-            <ion-icon name="settings"></ion-icon>
-            <ion-label>Ajustes</ion-label>
-        </ion-tab-button>
-    </ion-tab-bar>
+
+  <ion-tab-bar slot="bottom" translucent="true" class="custom-tab-bar">
+    
+    <ion-tab-button tab="inicio" href="?page=inicio">
+      <ion-icon name="home-outline" class="tab-icon"></ion-icon>
+      <ion-label>Inicio</ion-label>
+    </ion-tab-button>
+
+    <ion-tab-button tab="perfil" href="?page=perfil">
+      <ion-icon name="person-circle-outline" class="tab-icon"></ion-icon>
+      <ion-label>Perfil</ion-label>
+    </ion-tab-button>
+
+    <ion-tab-button tab="tiempos" href="?page=tiempos">
+      <ion-icon name="hourglass-outline" class="tab-icon"></ion-icon>
+      <ion-label>Tiempos</ion-label>
+    </ion-tab-button>
+
+    <ion-tab-button tab="configuracion" href="?page=configuracion">
+      <ion-icon name="settings-outline" class="tab-icon"></ion-icon>
+      <ion-label>Ajustes</ion-label>
+    </ion-tab-button>
+
+  </ion-tab-bar>
 </ion-tabs>
+
+<style>
+  ion-tab-bar.custom-tab-bar {
+    background: rgba(255, 255, 255, 0.9);
+    backdrop-filter: blur(8px);
+    border-top: 1px solid #ddd;
+    --color: #666;
+    --color-selected: #3b82f6;
+    --background: #fff;
+  }
+
+  ion-tab-button {
+    transition: all 0.3s ease;
+  }
+
+  ion-tab-button.ion-selected {
+    transform: scale(1.1);
+    font-weight: bold;
+  }
+
+  .tab-icon {
+    transition: transform 0.3s ease;
+  }
+
+  ion-tab-button.ion-selected .tab-icon {
+    transform: scale(1.3) rotate(10deg);
+    color: #3b82f6;
+  }
+</style>
