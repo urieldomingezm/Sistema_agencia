87bbb50a920cbd74e8cc19d11b3e236b5bee82fe
Angel uriel dominguez medina <urielmedina7u7@outlook.com>
2025-06-06 16:31:56 -0600

    Update header_app.php

--- sistema_agencia/private/plantilla/header_app.php
+++ sistema_agencia/private/plantilla/header_app.php
@@ -5,16 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title><?php echo isset($pageTitle) ? $pageTitle : 'Agencia Shein APP'; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/framework7@6.3.0/css/framework7.bundle.min.css">
-    <style>
-        html, body {
-            height: 100%;
-            margin: 0;
-            padding: 0;
-        }
-        .page {
-            min-height: 100vh;
-        }
-    </style>
 </head>
 <body>
     <div class="page">
