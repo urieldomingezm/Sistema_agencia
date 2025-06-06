814df2507db95c951e6fba098105af30e00671e7
TheRussian7u7 <urielmedina7u7@outlook.com>
2025-06-06 07:20:23 -0600

    Update index.php

--- sistema_agencia/usuario/app/index.php
+++ sistema_agencia/usuario/app/index.php
@@ -4,11 +4,9 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once(TEMPLATES_APP_PATH . 'header_app.php');
 ?>
 
-<ion-content fullscreen="true">
-    <div class="inner-scroll">
+
         <?php require_once(BIENVENIDA_APP_PATH . 'home_inicio.php'); ?>
-    </div>
-</ion-content>
+
 
 <?php
 require_once(TABS_APP_PATH . 'tab_bajos.php');
