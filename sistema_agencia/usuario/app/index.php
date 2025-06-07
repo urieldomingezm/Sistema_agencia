4706b2fbb3756101da118a998a1b76b745467cdd
TheRussian7u7 <urielmedina7u7@outlook.com>
2025-06-06 20:46:44 -0600

    Update index.php

--- sistema_agencia/usuario/app/index.php
+++ sistema_agencia/usuario/app/index.php
@@ -2,43 +2,9 @@
 $pageTitle = "Agencia Shein APP";
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once(TEMPLATES_APP_PATH . 'header_app.php');
-
-class AppController
-{
-    private $validPages = [
-        'inicio' => ['file' => 'home_inicio.php', 'roles' => ['all']],
-        'perfil' => ['file' => 'perfil.php', 'roles' => ['all']],
-        'notificaciones' => ['file' => 'notificaciones.php', 'roles' => ['all']],
-        'tiempos' => ['file' => 'tiempos.php', 'roles' => ['Logistica', 'Supervisor', 'Director']],
-        'ascensos' => ['file' => 'ascensos.php', 'roles' => ['Logistica', 'Supervisor', 'Director']],
-        'configuracion' => ['file' => 'configuracion.php', 'roles' => ['all']]
-    ];
-
-    public function loadContent()
-    {
-        $page = $_GET['page'] ?? 'inicio';
-
-        if (array_key_exists($page, $this->validPages)) {
-            $pageConfig = $this->validPages[$page];
-
-            if ($pageConfig['roles'][0] === 'all') {
-                if ($page === 'perfil') {
-                    return $pageConfig['file'];
-                }
-                return BIENVENIDA_APP_PATH . $pageConfig['file'];
-            }
-        }
-
-        return BIENVENIDA_APP_PATH . 'home_inicio.php';
-    }
-}
-
-$controller = new AppController();
-$contentFile = $controller->loadContent();
 ?>
 
-<!-- Contenido principal usando Tailwind -->
-    <?php require_once($contentFile); ?>
+<?php require_once(BIENVENIDA_APP_PATH . 'home_inicio.php');?>
 
 <?php
 require_once(TABS_APP_PATH . 'tab_bajos.php');
