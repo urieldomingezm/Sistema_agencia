a140078e617f4277ae021dcd60b38c4f56f7467c
TheRussian7u7 <urielmedina7u7@outlook.com>
2025-06-14 08:13:06 -0600

    Update menu_administrativo.php

--- sistema_agencia/private/menus/menu_administrativo.php
+++ sistema_agencia/private/menus/menu_administrativo.php
@@ -85,9 +85,8 @@ class AdminNavbar
                 <li>
                   <hr class="dropdown-divider">
                 </li>
-                <li><a class="dropdown-item py-2" href="index.php?page=ver_perfil"><i class="bi bi-person-lines-fill me-2"></i> Perfil</a></li>
-                <li><a class="dropdown-item py-2" href="index.php?page=configuracion"><i class="bi bi-gear-fill me-2"></i> Modo movil</a></li>
-                <li><a class="dropdown-item py-2" href="index.php?page=configuracion"><i class="bi bi-gear-fill me-2"></i> Modo escritorio</a></li>
+                <li><a class="dropdown-item py-2" href="index.php?page=configuracion"><i class="bi bi-phone me-2"></i> Modo movil</a></li>
+                <li><a class="dropdown-item py-2" href="index.php?page=configuracion"><i class="bi bi-display me-2"></i> Modo escritorio</a></li>
                 <li>
                   <hr class="dropdown-divider my-1">
                 </li>
